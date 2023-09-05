d03cc321ef87588b74fac64ef5c7003962a93c22
ankit-kumar164 <ankit.kumar614@gmail.com>
2023-09-05 22:14:57 +0530

    second commit

---
+++ db/conn.mjs
@@ -0,0 +1,23 @@
+import { MongoClient } from "mongodb";
+
+const connectionString = process.env.ATLAS_URI || "";
+
+const client = new MongoClient(connectionString);
+
+let conn;
+try {
+  conn = await client.connect();
+  console.log("Connected to MongoDB Atlas...");
+} catch(e) {
+  console.error(e);
+}
+let db = conn.db("Ankit");
+// db.createCollection("records", function(err, res) {
+//     if (err){
+//         console.log(err)
+//     } else {
+//         console.log("Collection is created!");
+//     }}
+// )
+
+export default db;
