f3a3bd77fdceb7a732cd2f133c0f406389efb814
ankit-kumar164 <ankit.kumar614@gmail.com>
2023-09-06 17:23:56 +0530

    modified:   client/build/asset-manifest.json 	modified:   client/build/index.html 	deleted:    client/build/static/js/main.68dde271.js 	deleted:    client/build/static/js/main.68dde271.js.LICENSE.txt 	deleted:    client/build/static/js/main.68dde271.js.map 	modified:   client/package-lock.json 	modified:   client/src/components/create.js 	modified:   client/src/components/edit.js 	modified:   client/src/components/recordList.js 	modified:   db/conn.mjs 	client/build/static/js/

--- db/conn.mjs
+++ db/conn.mjs
@@ -1,5 +1,6 @@
 import { MongoClient } from "mongodb";
-
+import dotenv from "dotenv";
+dotenv
 const connectionString = process.env.ATLAS_URI || "";
 
 const client = new MongoClient(connectionString);
